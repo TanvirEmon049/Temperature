695bd095a94bbfb26006bcf9cfed08124c70bede
TanvirEmon049 <122195598+TanvirEmon049@users.noreply.github.com>
2023-03-05 00:22:59 +0600

    Add files via upload

---
+++ temperature-hot/js/app.js
@@ -0,0 +1,35 @@
+const API_KEY = `7a8d0acc1f1be54ece8f994d3d242f09`
+
+const loadTemperature = city =>{
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayTemperature(data));
+}
+
+const displayTemperature = data =>{
+    /* const temperature = document.getElementById('temperature');
+    // console.log(data.main.temp)
+    temperature.innerText = data.main.temp; */
+
+    setInnerTextById('temperature', data.main.temp);
+    setInnerTextById('condition', data.weather[0].main);
+    console.log(data.weather[0].main);
+}
+
+const setInnerTextById = (id, text) => {
+    const temperature = 
+    document.getElementById(id);
+    temperature.innerText = text;
+}
+
+document.getElementById('btn-search').addEventListener('click', function(){
+    const searchField = document.getElementById('search-field');
+    const city = searchField.value;
+    // set city
+    document.getElementById('city').innerText = city;
+
+    loadTemperature(city);
+})
+
+loadTemperature('dhaka')
